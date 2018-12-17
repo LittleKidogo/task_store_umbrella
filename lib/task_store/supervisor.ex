aae7b5c21bceca7ad645bd8f67eae232ea57b370
Zacck Osiemo <coderv63@gmail.com>
2018-12-17 18:18:26 +0200

    implement a supervisor for the application

---
+++ lib/task_store/supervisor.ex
@@ -0,0 +1,26 @@
+defmodule TaskStore.Supervisor do 
+  @moduledoc """
+  This module holds the implentation logic for the supervision tree ued to handle the Registry Process
+  """
+  use Supervisor
+
+  @doc """
+  This function starts a supervisor in charge of monitoring a few processes in `TaskStore`
+  """
+  @spec start_link(list(any())) :: {atom(), pid()}
+  def start_link(opts) do 
+    Supervisor.start_link(__MODULE__, :ok, opts)
+  end 
+
+  @doc """
+  This function runs the work needed to initialize our supervisor when starting it
+  """
+  def init(:ok) do 
+    children = [
+      {TaskStore.Registry, name: TaskStore.Registry}
+    ]
+
+    # set the strategy to `one_for_one` so whichever child that dies is the only one that is restarted
+    Supervisor.init(children, strategy: :one_for_one)
+  end 
+end 
