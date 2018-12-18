a768d3a92585e08e6b05129644d3b64da1f31e95
Zacck Osiemo <coderv63@gmail.com>
2018-12-18 10:35:34 +0200

    swap tests to use ets

--- test/lib/registry_test.exs
+++ test/lib/registry_test.exs
@@ -8,10 +8,10 @@ defmodule TaskStore.RegistryTest do
     TaskList
   }
 
-  setup do 
+  setup context do 
     # use start supervised to ensure that the process is shutdown before the next test 
-    registry = start_supervised!(Registry)
-    %{registry: registry}
+    _ = start_supervised!({Registry, name: context.test})
+    %{registry: context.test}
   end 
 
   @task_list "shopping"
@@ -32,6 +32,7 @@ defmodule TaskStore.RegistryTest do
       Registry.create(registry, @task_list)
       {:ok, task_list} = Registry.lookup(registry, @task_list)
       Agent.stop(task_list)
+      Registry.create(registry, "not_used")
       assert Registry.lookup(registry, @task_list) == :error
     end
 
@@ -41,7 +42,18 @@ defmodule TaskStore.RegistryTest do
 
       # muahahaha crash the TaskList 
       Agent.stop(task_list, :shutdown)
+      Registry.create(registry, "not_used")
       assert Registry.lookup(registry, @task_list) == :error
     end  
+
+    test "task_list can crash any time", %{registry: registry} do 
+      Registry.create(registry, @task_list)
+      {:ok, task_list} = Registry.lookup(registry, @task_list)
+
+      # Simulate a task list crash 
+      Agent.stop(task_list, :crash)
+
+      catch_exit TaskList.put(task_list, @task, @task_time)
+    end 
   end 
 end 
