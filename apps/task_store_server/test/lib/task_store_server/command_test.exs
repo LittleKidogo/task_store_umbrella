08b146d32c2f3e0406e1011f891eafa4af46e80f
Zacck Osiemo <coderv63@gmail.com>
2018-12-19 12:45:13 +0200

    run command parser tests

---
+++ apps/task_store_server/test/lib/task_store_server/command_test.exs
@@ -0,0 +1,7 @@
+defmodule TaskStoreServer.CommandTest do 
+    @moduledoc """
+    This module holds unit tests for the functions in the `TaskStoreServer.Command` boundary module
+    """
+    use ExUnit.Case, async: true 
+    doctest TaskStoreServer.Command
+end 
