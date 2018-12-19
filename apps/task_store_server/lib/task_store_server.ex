137761811432e2700fe3917e52b00c7fb1a0ea1f
Zacck Osiemo <coderv63@gmail.com>
2018-12-19 10:45:55 +0200

    supervise client connections using a task

--- apps/task_store_server/lib/task_store_server.ex
+++ apps/task_store_server/lib/task_store_server.ex
@@ -23,7 +23,8 @@ defmodule TaskStoreServer do
   @spec loop_acceptor(any()) :: any() 
   defp loop_acceptor(socket) do 
     {ok, client} = :gen_tcp.accept(socket)
-    serve(client)
+    {:ok, pid} = Task.Supervisor.start_child(TaskStoreServer.TaskSupervisor, fn -> serve(client) end)
+    :ok  = :gen_tcp.controlling_process(client, pid)
     loop_acceptor(socket)
   end 
 
