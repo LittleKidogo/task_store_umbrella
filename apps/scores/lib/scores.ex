f241b0af6c1333e3eabce48fb404e9476e3c0b1d
br4inii <moshericoe@gmail.com>
2019-01-30 00:31:42 +0300

    add more functionality to agent function and format code

--- apps/scores/lib/scores.ex
+++ apps/scores/lib/scores.ex
@@ -1,11 +1,14 @@
 defmodule Scores do
   def start do
-    Agent.start_link(fn -> %{lifetime_score: 0, current_score: 0, last_played: "never"} end)
+    Agent.start_link(fn ->
+      %{score: 0, best_score: 0, previous_score: "never played", lifetime_score: 0}
+    end)
   end
 
   def put(pid, key, value) do
-    Agent.update(pid, &Map.put(&1, key, value))
-    overall_score(pid, key, value)
+    save_score(pid)
+    Agent.update(pid, fn state -> Map.put(state, key, value) end)
+    calculate_scores(pid, key, value)
   end
 
   def get(pid, key) do
@@ -16,21 +19,28 @@ defmodule Scores do
     Agent.get(pid, fn state -> state end)
   end
 
-  defp overall_score(pid,key, value) do
+  defp calculate_scores(pid, key, value) do
     case key do
-      :current_score ->
-        last_played = Agent.get(pid, &Map.get(&1, :current_score))
-
+      :score ->
         overall_score = Agent.get(pid, &Map.get(&1, :lifetime_score)) + value
-        IO.puts("overall = #{overall_score}\n value = #{value}\n last_played = #{last_played}")
-
-        # update state
+        best_score(pid, value)
         Agent.update(pid, &Map.put(&1, :lifetime_score, overall_score))
-        Agent.update(pid, &Map.put(&1, :last_played, last_played))
-
 
       _ ->
+        :ok
+    end
+  end
 
+  defp save_score(pid) do
+    scores_bfr_update = Agent.get(pid, &Map.get(&1, :score))
+    Agent.update(pid, &Map.put(&1, :previous_score, scores_bfr_update))
+  end
+
+  defp best_score(pid, value) do
+    current_best_score = Agent.get(pid, fn state -> Map.get(state, :best_score) end)
+
+    if is_integer(value) and current_best_score < value do
+      Agent.update(pid, fn state -> Map.put(state, :best_score, value) end)
     end
   end
 end
